249e95ec64867850dfa0bb528484ad6cb0065a08
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-12-18 16:44:21 -0500

    Add Vue app and HTML template

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'the final empire',
+            author: 'Brandon Sanderson',
+            age: 45,
+            
+        }
+    }
+    // template: "<h2>This is the template.</h2>",
+})
+
+app.mount("#app")
